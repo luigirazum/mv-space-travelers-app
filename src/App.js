cf641876e420b4c1a0da982d68af62fd0c16b791
Luis Zubia <soyzubia@gmail.com>
2023-02-13 23:53:26 -0600

    Add route to /myprofile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header/Header';
+import MyProfile from './pages/MyProfile';
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <main>
-      <p>Content here.</p>
+      <Routes>
+        <Route caseSensitive path="myprofile" element={<MyProfile />} />
+      </Routes>
     </main>
   </BrowserRouter>
 );
