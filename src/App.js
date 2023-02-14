41fa3cd9bba31a22ad3edbc3c87340302e98f623
Luis Zubia <soyzubia@gmail.com>
2023-02-13 23:59:24 -0600

    Add * route for invalid paths

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header/Header';
 import MyProfile from './pages/MyProfile';
+import NotFound from './pages/NotFound';
 
 const App = () => (
   <BrowserRouter>
@@ -9,6 +10,7 @@ const App = () => (
     <main>
       <Routes>
         <Route caseSensitive path="myprofile" element={<MyProfile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
   </BrowserRouter>
