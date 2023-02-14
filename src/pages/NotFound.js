02b8f0293581d549a363c0dd97f8bfe647ca0e80
Luis Zubia <soyzubia@gmail.com>
2023-02-13 23:58:35 -0600

    Add <NotFound> page for and invalid route

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <div>
+      <span>An error has ocurred:</span>
+      <p>The page was not found.</p>
+    </div>
+  </>
+);
+
+export default NotFound;
