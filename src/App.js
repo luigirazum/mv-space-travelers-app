53eda383c2fd25d0f358e61c17170532679d392f
Luis Zubia <soyzubia@gmail.com>
2023-02-14 11:01:49 -0600

    Add Rockets page with no content, Add route for root path wich points to Rockets page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header/Header';
-import MyProfile from './pages/MyProfile';
 import NotFound from './pages/NotFound';
+import MyProfile from './pages/MyProfile';
+import Rockets from './pages/Rockets';
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <main>
       <Routes>
+        <Route index element={<Rockets />} />
         <Route caseSensitive path="myprofile" element={<MyProfile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/pages/Rockets.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Rockets = () => (
+  <>
+    <h2>🚀 Rockets</h2>
+    <p>Under Construction 👷‍♂️</p>
+  </>
+);
+
+export default Rockets;
