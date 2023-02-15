a671536093233d61bd79aaaf85b40553688af37d
Luis Zubia <soyzubia@gmail.com>
2023-02-15 00:40:05 -0600

    Add Missions view component

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/missions/missions';
 
-const Missions = () => (
-  <>
-    <h2>⛔ No Missions available right now.</h2>
-    <p>Under construction 👷‍♂️</p>
-  </>
-);
+const Missions = () => {
+  const status = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <h2>⛔ No Missions available right now.</h2>
+      <p>Under construction 👷‍♂️</p>
+      <button
+        type="button"
+        onClick={() => dispatch(checkStatus())}
+      >
+        Check Status
+      </button>
+      {status}
+    </>
+  );
+};
 
 export default Missions;
