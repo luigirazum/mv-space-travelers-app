f0dcb729198b79c914bdc5874fb665657a6e0dd1
Luis Zubia <soyzubia@gmail.com>
2023-02-16 14:28:54 -0600

    Add conditional rendering to the reserved badge, book rocket and cancel reservation

--- src/components/Rocket/Rocket.js
+++ src/components/Rocket/Rocket.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { reserveRocket, cancelReservedRocket } from '../../redux/rockets/rockets';
 import './Rocket.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { cancelReservedRocket, reserveRocket } from '../../redux/rockets/rockets';
 
-const Rocket = (props) => {
-  const {
-    rocketData: rocket,
-    rocketData: {
-      name, description, image,
-    },
-  } = props;
+const selectRocketById = (state, rocketId) => state
+  .rockets.inventory.find((rocket) => rocket.id === rocketId);
+
+const Rocket = ({ id }) => {
+  const rocket = useSelector((state) => selectRocketById(state, id));
 
   const dispatch = useDispatch();
+
+  const handleBookRocket = (id) => {
+    dispatch(reserveRocket(id));
+  };
+
+  const handleCancelReservedRocket = (id) => {
+    dispatch(cancelReservedRocket(id));
+  };
+
+  const { name, description, images } = rocket;
+  const [image] = images;
 
   return (
     <article className="rocketItem">
@@ -20,34 +29,37 @@ const Rocket = (props) => {
         <img className="rocketImage" src={image} alt={name} />
       </div>
       <div className="rocketInfo">
-        <h4 className="rocketName">{name}</h4>
+        <h4 className="rocketName">
+          {name}
+          {rocket.reserved
+            && <span className="badge rktBadge resBadge">Reserved</span>}
+        </h4>
         <p className="rocketDescription">{description}</p>
-        <button
-          type="button"
-          className="btn btn-rReserve"
-          onClick={() => dispatch(reserveRocket(rocket.id))}
-        >
-          Book this Rocket
-        </button>
-        <button
-          type="button"
-          className="btn btn-cReserve"
-          onClick={() => dispatch(cancelReservedRocket(rocket.id))}
-        >
-          Cancel Reservation
-        </button>
+        {!rocket.reserved ? (
+          <button
+            type="button"
+            className="btn btn-rReserve"
+            onClick={() => handleBookRocket(id)}
+          >
+            Book this Rocket
+          </button>
+        )
+          : (
+            <button
+              type="button"
+              className="btn btn-cReserve"
+              onClick={() => handleCancelReservedRocket(id)}
+            >
+              Cancel Reservation
+            </button>
+          )}
       </div>
     </article>
   );
 };
 
 Rocket.propTypes = {
-  rocketData: PropTypes.exact({
-    id: PropTypes.string,
-    name: PropTypes.string,
-    description: PropTypes.string,
-    image: PropTypes.string,
-  }).isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Rocket;
