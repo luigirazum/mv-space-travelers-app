c7959dc03d5e4fe9b190845e200728a2d6f6c865
Luis Zubia <soyzubia@gmail.com>
2023-02-15 18:54:37 -0600

    Add actions and logic to missions' reducer to fetch the missions from the API

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,35 +1,95 @@
+import { createAction, createSlice } from '@reduxjs/toolkit';
+import asyncFetch, { GET_MISSIONS } from '../api/apiSettings';
+
 /**
  * action types for MISSIONS
  */
 const PREFIX = 'spaceTravelers/missions';
-const CHECK_STATUS = `${PREFIX}/CHECK_STATUS`;
-
-/**
- * initialState for MISSIONS
- */
-const initialState = [];
-
-/**
- * reducer for MISSIONS
- */
-const reducer = (state = initialState, action = {}) => {
-  switch (action.type) {
-    case CHECK_STATUS:
-      return (
-        <>
-          <p>We are working hard on new Missions. 👩‍🚀</p>
-          <p>We&apos;ll be ready for launch very soon. 🚀</p>
-        </>
-      );
-    default:
-      return state;
-  }
-};
+const CHECK_MISSIONS_STATUS = `${PREFIX}/CHECK_MISSIONS_STATUS`;
+const FETCH_MISSIONS = `${PREFIX}/FETCH_MISSIONS`;
 
 /**
  * action creators for MISSIONS
  */
-const checkStatus = () => ({ type: CHECK_STATUS });
+const checkMissionsStatus = createAction(CHECK_MISSIONS_STATUS);
 
-export { checkStatus };
+/**
+ * get the Missions available from the API
+ */
+const fetchMissions = asyncFetch(FETCH_MISSIONS, GET_MISSIONS);
+
+const initialState = {
+  avaliable: [],
+  loading: false,
+  error: null,
+};
+
+/**
+ * slicer for MISSIONS
+ */
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: { },
+  extraReducers: (builder) => {
+    builder
+      .addCase(
+        fetchMissions.pending,
+        (state) => ({
+          ...state,
+          loading: true,
+          error: null,
+        }),
+      )
+      .addCase(
+        fetchMissions.fulfilled,
+        (state, action) => {
+          const { payload } = action;
+          const selectData = payload.map((mission) => {
+            const {
+              mission_id: id, mission_name: name, description,
+            } = mission;
+            return {
+              id, name, description,
+            };
+          });
+          return {
+            ...state,
+            avaliable: selectData,
+            loading: false,
+          };
+        },
+      )
+      .addCase(
+        fetchMissions.rejected,
+        (state, action) => {
+          const { message } = action.error;
+          return {
+            ...state,
+            loading: false,
+            error: message,
+          };
+        },
+      )
+      .addCase(
+        checkMissionsStatus,
+        (state) => {
+          const { avaliable } = state;
+          const message = avaliable
+            ? 'We are working hard on new Missions. 👩‍🚀 We\'ll be ready for launch very soon. 🚀'
+            : null;
+          return {
+            ...state,
+            message,
+          };
+        },
+      );
+  },
+});
+
+/** actions available for Missions */
+export { fetchMissions, checkMissionsStatus };
+
+/** export reducer for missions */
+const { reducer } = missionsSlice;
 export default reducer;
